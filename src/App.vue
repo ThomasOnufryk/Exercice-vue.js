d12fdb3de0a053d6aae91d522989807288b0c5ab
ThomasOnufryk <thomas.onufryk@gmail.com>
2025-08-07 14:33:04 +0200

    Correction v-model et template inutile

--- src/App.vue
+++ src/App.vue
@@ -2,18 +2,14 @@
   <div class="block">
     <div class="list">
       <h1>Mes Articles</h1>
-      <template v-if="articles.length">
-        <div class="articles-list">
+        <div v-if="articles.length" class="articles-list">
           <ul>
             <li v-for="article in articles" @click="setCurrentArticle(article)" :key="article.id">
-              <ArticleList :class="{ selected: currentIndex === article.id }" :article="article" />
+              <ArticleItem :class="{ selected: currentIndex === article.id }" :article="article"/>
             </li>
           </ul>
         </div>
-      </template>
-      <template v-else>
-        <h3>Vous n'avez pas encore enregistré d'articles dans votre liste</h3>
-      </template>
+        <h3 v-else >Vous n'avez pas encore enregistré d'articles dans votre liste</h3>
     </div>
 
     <ArticleForm v-model:article="currentArticle" v-model:editionMode="editionMode" @save-article="saveArticle"
@@ -24,16 +20,16 @@
 <script setup>
 import { ref } from "vue";
 import ArticleForm from "@/components/ArticleForm.vue";
-import ArticleList from "@/components/ArticleList.vue";
+import ArticleItem from "@/components/ArticleItem.vue";
 
 const articles = ref([
-  { id: 1, name: "Achat de matériel", price: 20.0, vat: 20.0 },
-  { id: 2, name: "Dépose des murs", price: 150.0, vat: 20.0 },
+  {id: 1, name: "Achat de matériel", price: 20.0, vat: 20.0},
+  {id: 2, name: "Dépose des murs", price: 150.0, vat: 20.0},
 ]);
 
 const editionMode = ref(false);
 const currentArticle = ref({});
-const currentIndex = ref(-1);
+const currentIndex = ref(null);
 
 const setCurrentArticle = (article) => {
   currentArticle.value = { ...article };
@@ -44,27 +40,24 @@ const setCurrentArticle = (article) => {
 const saveArticle = () => {
   if (editionMode.value && currentIndex.value >= 0) {
     const articleIndex = articles.value.findIndex(
-      (a) => a.id === currentIndex.value
+        (a) => a.id === currentIndex.value
     );
-    articles.value[articleIndex] = { ...currentArticle.value };
-    console.log(articles.value);
+    articles.value[articleIndex] = {...currentArticle.value};
+    console.log("UPDATE", articles.value);
   } else {
     const articleId = articles.value.length
-      ? Math.max(...articles.value.map((a) => a.id)) + 1
-      : 1;
-    articles.value.push({ id: articleId, ...currentArticle.value });
-    console.log(articles.value);
+        ? Math.max(...articles.value.map((a) => a.id)) + 1
+        : 1;
+    articles.value.push({id: articleId, ...currentArticle.value});
+    console.log("CREATION", articles.value);
   }
-
-  editionMode.value = false;
-  currentArticle.value = {};
-  currentIndex.value = -1;
+  newArticle();
 };
 
 const newArticle = () => {
   editionMode.value = false;
   currentArticle.value = {};
-  currentIndex.value = -1;
+  currentIndex.value = null;
 };
 </script>
 
